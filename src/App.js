0447a6d9951476e80f0e5aa00a445c59839cb05b
ross-u <cirkovic.uros.srb@gmail.com>
2021-07-02 16:57:01 +0200

    Example - Function and Class components 3

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import "./App.css";
+import Greeting from './components/Greeting';
 
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <Greeting />
+    </div>
   );
 }
 
