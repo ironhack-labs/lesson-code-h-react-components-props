cd5c90b4fda433b16455a257f64486018c1903c1
ross-u <cirkovic.uros.srb@gmail.com>
2021-07-02 16:55:28 +0200

    Example - Function and Class components 2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,8 @@
-import logo from "./logo.svg";
 import "./App.css";
-import ExampleFuncA from "./components/ExampleFuncA";
-import ExampleFuncB from "./components/ExampleFuncB";
-import ExampleClassA from "./components/ExampleClassA";
-import ExampleClassB from "./components/ExampleClassB";
 
 function App() {
   return (
-    <div className="App">
-      <ExampleFuncA />
-      <ExampleFuncB />
-      <ExampleClassA />
-      <ExampleClassB />
-    </div>
+    <div className="App"></div>
   );
 }
 
