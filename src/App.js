7b5a1b36c96f2b301cb7b48821fa9fe4dbdc3c68
ross-u <cirkovic.uros.srb@gmail.com>
2021-07-03 22:33:21 +0200

    Example - Function and Class components 4

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Navbar from './src/components/Navbar';
 import Greeting from './components/Greeting';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Greeting />
     </div>
   );
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Navbar extends React.Component {
+  render() {
+    return (
+      <nav>
+        <p>React - Components & props</p>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
+
