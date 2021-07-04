e9687f2e30673afdd631e8559d66904cbcfde628
ross-u <cirkovic.uros.srb@gmail.com>
2021-07-04 19:07:51 +0200

    Example - props.children

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import "./App.css";
 import Navbar from './components/Navbar';
 import Greeting from './components/Greeting';
+import StudentCard from './components/StudentCard';
+import StudentList from './components/StudentList';
+
+
 
 function App() {
+
   return (
     <div className="App">
       <Navbar />
       <Greeting firstName="Harper" />
-      <hr />
       <Greeting firstName="Michelle" />
-      <hr />
       <Greeting firstName="Andrea" />
-      <hr />
+      <StudentList>
+        <StudentCard name="Eva" week={7} info={{ city: 'BCN', course: "WEB" }} />
+        <StudentCard name="Mat" week={8} info={{ city: 'MIA', course: "DATA" }} />
+      </StudentList>
     </div>
   );
 }
